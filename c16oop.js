06017b94bff5b87ce0b07f0345c4ae2950db84e2
Eko-Prasetyo15 <prasetyoeko822@gmail.com>
2020-02-19 16:56:40 +0700

    chalange16 done

--- c16oop.js
+++ c16oop.js
@@ -1,45 +1,94 @@
 class carFactory{
-constructor(){
-    this.number = [(Math.floor(Math.random() * 5) + 1)]
-}
+    constructor(){
+        this.number = [(Math.floor(Math.random() * 2) + 1)]
+    }
 buildup(){
-    let hasil =[]
-    let bmw = new BMW(1,1)
-    for (let i = 0; i < this.number; i++){
-        hasil.push(bmw.assemble());
+    let hasil = []
+    let daihatsu = new DAIHATSU();
+    let toyota = new TOYOTA();
+    let suzuki = new SUZUKI();
+    let honda = new HONDA();
+    for (let i = 0; i < this.number ; i++){
+        hasil.push(daihatsu.assemble())
+        hasil.push(toyota.assemble())
+        hasil.push(suzuki.assemble())
+        hasil.push(honda.assemble())
     }
     console.log(hasil)
 }
 }
+
 class Car{
-constructor(dashboard, bagagge){
-    this.dashboard = dashboard
-    this.bagagge = bagagge
-    this.tyre = new Tyre();
-}
-warranty(){
-    let warrantytime = [(Math.floor(Math.random() * 5) + 1)]
-    return warrantytime;
-}
-}
-class Tyre{
-    constructor(){
-        let merk = ['Dunlop','Bridgestone','GT-radial','Falken','Yokohama'];
-        this.defaulttyre = merk [(Math.floor(Math.random() * 4) + 1)]
+    constructor(serviceBook, warrantyBook, toolsKitbox ){
+        this.serviceBook = 1;
+        this.warrantyBook = 1;
+        this.toolsKitbox = 1;
+        this.tyre = new Tyre();
+    }
+    warranty(){
+        let warrantyTime = [(Math.floor(Math.random() * 4) + 2)]
+        return warrantyTime;
     }
 }
-class BMW extends Car{
+class Tyre{
+constructor(){
+    let merk = ['Dunlop','Bridgestone','GT-Radial','Swalow']
+    this.defaulttyre = merk[(Math.floor(Math.random() * 3) + 1)]
+
+}
+}
+class DAIHATSU extends Car{
     assemble(){
-        let newBmw = {
-            type : 'i8', 
-            dashboard : `${this.dashboard}`,
-            bagagge : `${this.bagagge}`,
+        let newdaihatsu = {
+            type : 'Xenia', 
+            serviceBook : `${this.serviceBook}`,
+            warrantyBook : `${this.warrantyBook}`,
+            toolsKitbox : `${this.toolsKitbox}`,
             tyre : `${this.tyre.defaulttyre}`, 
             garansi : `${this.warranty()} tahun`
         }
-        return newBmw
+        return newdaihatsu
     }
-
 }
+class TOYOTA extends Car{
+    assemble(){
+        let newtoyota = {
+            type : 'Avanza', 
+            serviceBook : `${this.serviceBook}`,
+            warrantyBook : `${this.warrantybook}`,
+            toolsKitbox : `${this.toolsKitbox}`,
+            tyre : `${this.tyre.defaulttyre}`, 
+            garansi : `${this.warranty()} tahun`
+        }
+        return newtoyota
+    }
+}
+class SUZUKI extends Car{
+    assemble(){
+        let newsuzuki = {
+            type : 'Ertiga', 
+            serviceBook : `${this.serviceBook}`,
+            warrantyBook : `${this.warrantybook}`,
+            toolsKitbox : `${this.toolsKitbox}`,
+            tyre : `${this.tyre.defaulttyre}`, 
+            garansi : `${this.warranty()} tahun`
+        }
+        return newsuzuki
+    }
+}
+class HONDA extends Car{
+    assemble(){
+        let newhonda = {
+            type : 'Mobilio', 
+            serviceBook : `${this.serviceBook}`,
+            warrantyBook : `${this.warrantybook}`,
+            toolsKitbox : `${this.toolsKitbox}`,
+            tyre : `${this.tyre.defaulttyre}`, 
+            garansi : `${this.warranty()} tahun`
+        }
+        return newhonda
+    }
+}
+
 let cars = new carFactory;
 cars.buildup();
